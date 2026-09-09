2ee9916e1f6345a3ce5fc17d22543457c9a98099
cocojrr-max <coco.jrr@gmail.com>
2026-09-09 01:10:56 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='burbupet-erp-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','styles.css','app.js','manifest.webmanifest']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
